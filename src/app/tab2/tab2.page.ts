11c9b469c2cd9c2dbeae59b1d21ec64dce38a014
JesusDFernandez <78115281+JesusDFernandez@users.noreply.github.com>
2023-11-10 17:50:35 -0400

    vista tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,49 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  httpClient = inject(HttpClient);
+  authService = inject(AuthService);
+  private router = inject(Router)
 
+
+
+  ngOnInit() {
+
+    this.authService.users.subscribe(response => {
+      console.log(response);
+      this.data = [];
+
+      this.data.push(response)
+      this.results.set(response);
+
+    })
+
+
+  }
+
+  data = <any>[];
+
+  results = signal<any>([this.data]);
+
+
+
+  handleInput(event: any) {
+    const query = event.target.value.toLowerCase();
+
+    console.log(query);
+    console.log(this.data);
+
+    this.results.set(this.data[0].filter((d: any) => d.username.toLowerCase().indexOf(query) > -1));
+
+
+
+  }
+
+  goToProfile(id: any) {
+    console.log(id);
+
+    this.router.navigate(['tabs/profile/' + id]);
+
+  }
 }
+
